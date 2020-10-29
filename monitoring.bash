3fadd3cf648b1cd802317830959ccfd60c266fc3
Henrikas <37771470+HenrikasR@users.noreply.github.com>
2020-10-30 00:32:57 +0200

    Update monitoring.bash

--- monitoring.bash
+++ monitoring.bash
@@ -4,9 +4,9 @@ while [ true ]
 do
 	if [[ $(systemctl is-active nginx) == "active" ]]
 	then
-		echo "Service is running"
+		echo "Service is running ($(date))" >> /home/ubuntu/logs.txt
 	else
-		echo "Starting the service"
+		echo "Starting the service ($(date))" >> /home/ubuntu/logs.txt
 		systemctl restart nginx
 		echo "Restart has happened at $(date)" >> /home/ubuntu/logs.txt
 	fi
